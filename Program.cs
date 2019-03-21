cdde227359e4770cf52b10175a377a8d4c767144
David Ståhl <47720480+DavidnStahl@users.noreply.github.com>
2019-03-21 17:40:30 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beach_time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many degrees is it outside? : ");
+            double degreesOutside = double.Parse(Console.ReadLine());
+            if (degreesOutside > 24)
+            {
+                Console.WriteLine("Beach time!");
+            }
+            else
+            {
+                Console.WriteLine("I should just stay inside today...");
+            }
+            Console.ReadLine();
+            //If the entry is greater than 24, "Beach time!" Should be displayed.
+            //Otherwise it should be shown "I should just stay inside today..."
+        }
+    }
+}
